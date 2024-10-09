20be47099d6c531e9be8f39e941128988f06b7d9
pepars-Rashid <peparsrashid@gmail.com>
2024-10-08 17:30:35 -0700

    dexie

--- src/app/dexie/page.jsx
+++ src/app/dexie/page.jsx
@@ -8,12 +8,16 @@ export default function page() {
     const addTask = async (task) => {
         await db.tasks.add(task);
     };
+    const deleteTask = async (id) => {
+        console.log(id)
+        await db.tasks.delete(id)
+    }
 
     return (
         <div>
             <h1>Task Manager</h1>
             <AddTaskForm onAddTask={addTask} />
-            <TaskList />
+            <TaskList onDeleteTask={deleteTask}/>
         </div>
     );
 }
--- src/components/TaskList .jsx
+++ src/components/TaskList .jsx
@@ -3,16 +3,31 @@ import React from 'react';
 import { useLiveQuery } from 'dexie-react-hooks';
 import { db } from '@/database/localDB'; // Ensure this path is correct
 
-export default function TaskList() {
+export default function TaskList({onDeleteTask}) {
     const tasks = useLiveQuery(() => db.tasks.toArray(), []);
-    
-    if (!tasks) return <div>Loading...</div>;
+
+    const handleDelete = (id)=>{
+        const intId = parseInt(id, 10)
+        onDeleteTask(intId)
+    }
+
+    if (!tasks) return <div>Loading...</div>
 
     return (
-        <ul>
+        <ul className='flex flex-col gap-[5px]'>
             {tasks.map(task => (
-                <li key={task.id}>
-                    {task.text} - {task.checked ? 'Done' : 'Pending'} - {task.time}
+                <li key={task.id} className='flex gap-[15px]'>
+                    {task.text} - {task.checked ? '✔' : '❌'} - {task.time} 
+                    <button
+                        name='id'
+                        value={task.id}
+                        onClick={(e)=>{
+                            e.preventDefault()
+                            handleDelete(e.target.value)
+                        }}
+                        className='border-[1px] border-gray-800 bg-red-600 p-[2px]'>
+                        Delete 🗑
+                    </button>
                 </li>
             ))}
         </ul>
